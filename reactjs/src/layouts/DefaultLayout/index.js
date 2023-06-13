159cc989857570ec0d3c776306a505aa588e1ac7
Khanh <khanhabcdd1@gmail.com>
2023-06-13 10:04:20 +0700

    design layout main page

--- reactjs/src/layouts/DefaultLayout/index.js
+++ reactjs/src/layouts/DefaultLayout/index.js
@@ -1,17 +1,27 @@
 import Header from '../components/Header';
 import Sidebar from "../components/Sidebar";
-import './DefaultLayout.css';
+
+import classNames from "classnames/bind";
+import styles from './DefaultLayout.module.scss';
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
   return (
-    <div className="wrapper">
+    <div className={cx('wrapper')}>
       <Header />
-      <div className="container">
+      <div className={cx('container')}>
         <Sidebar />
-        <div className="content">{children}</div>
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
 }
+
+
+
+
+
+
+
 
 export default DefaultLayout;
--- reactjs/src/layouts/components/Header/index.js
+++ reactjs/src/layouts/components/Header/index.js
@@ -1,8 +1,56 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Search from '../Search';
+import { faCircleUser, faBell, faCircleInfo } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
+import * as getDataUser from '../../../apiServices/getDataUser';
+const cx = classNames.bind(styles);
 function Header() {
+  const [currentUser,setCurrentUser]= useState(undefined);
+  useEffect(()=>{
+    const fetchApi = async ()=>{
+        const resultUser = await getDataUser.getDataUser();
+            setCurrentUser(resultUser);
+        }
+    fetchApi();
+},[]);
+
  return (
-   <div>
-      Header
-   </div>
+ <header className={cx('wrapper')}>
+  <div className={cx('content')}>
+        <Search/>
+        <div className={cx('actions')}>
+        { currentUser &&
+            <div className={cx('item-right')}>
+              <div className={cx('user-info')}>
+                  <img className={cx('avatar-user')} src={currentUser.img} alt='img-user'/>
+                  <span className={cx('name-user')}>{currentUser.name}</span>
+              </div>
+              <div className={cx('icon')}>
+                  <a href='/Notification'>
+                    <FontAwesomeIcon icon={faBell}/>
+                  </a>
+                  <a href='/Help'>
+                    <FontAwesomeIcon icon={faCircleInfo}/>
+                  </a>
+              </div>
+            </div>
+        }
+        { currentUser === false &&
+            <>
+              <button>
+                <a href={'http://localhost:3000/login'} >
+                  <FontAwesomeIcon icon={faCircleUser} />
+                    SIGN IN
+                </a>
+              </button>
+            
+         </>
+        }
+    </div>
+</div>  
+</header>
  )   
 }
 export default Header;
