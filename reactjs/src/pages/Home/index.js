e264ea6534083a45a57d11e77c7ba31f0a448291
hoxuanty <hoxuanty2003qng@gmail.com>
2023-06-19 15:43:25 +0700

    feat(home teacher): show class form API

--- reactjs/src/pages/Home/index.js
+++ reactjs/src/pages/Home/index.js
@@ -1,8 +1,13 @@
-function Home(){
+import ShowClass from "../ShowClass/ShowClass";
+import classNames from 'classnames/bind';
+import styles from '../ShowClass/Showclass.module.scss';
+const cx = classNames.bind(styles);
+
+function Home() {
 
     return (
-        <div>
-            home page hellu
+        <div className={cx('Home-teachers')}>
+            <ShowClass/>
         </div>
       
     )
