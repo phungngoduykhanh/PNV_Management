7164bc13c465b489a2a0d52de0aa2375ec550270
loanthiho <loan.hothi24@student.passerellesnumeriques.org>
2023-06-23 23:28:17 +0700

    calendar

--- reactjs/src/routes/index.js
+++ reactjs/src/routes/index.js
@@ -3,11 +3,15 @@ import { WithoutLayout } from "../layouts";
 import Home from "../pages/Home/";
 import Login from "../pages/Login/Login";
 import Register from '../pages/Register/index';
+import MyCalendar from "../pages/Calendar/Calendar";
+import Navigation from "../pages/Calendar/Calendar";
 
 const publicRoutes = [
   { path: "/", component: Home },
   { path: "/Login", component: Login, layout: WithoutLayout },
-  { path: "/Register", component: Register, layout: WithoutLayout }
+  { path: "/Register", component: Register, layout: WithoutLayout },
+  { path: "/Calendar", component: MyCalendar, layout: WithoutLayout },
+ 
 
 ];
 
