b4faf3d68f25d9f7ebb86cac896392e031d86c60
loanthiho <loan.hothi24@student.passerellesnumeriques.org>
2023-07-04 19:51:39 +0700

    profile

--- reactjs/src/pages/Profile/Userprofile.js
+++ reactjs/src/pages/Profile/Userprofile.js
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import './Userprofile.css'; // Import the CSS file for styling
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./User.css";
 import axios from 'axios';
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBBtn,
+  MDBBreadcrumb,
+  MDBBreadcrumbItem,
+  MDBProgress,
+  MDBProgressBar,
+  MDBIcon,
+  MDBListGroup,
+  MDBListGroupItem
+} from 'mdb-react-ui-kit';
 
-const UserProfileEdit = () => {
-  
+const UserProfile = () => {
+
   const [user, setUser] = useState({
     name: '',
     phone: '',
@@ -13,8 +31,8 @@ const UserProfileEdit = () => {
     role: '', 
   });
 
-  useEffect(()=>{
-    const token = localStorage.getItem('token'); 
+  useEffect(() => {
+    const token = localStorage.getItem('token');
     console.log(token);
     if (token) {
       axios
@@ -30,26 +48,24 @@ const UserProfileEdit = () => {
           console.error(error);
         });
     }
-  },[])
+  }, []);
 
   const [editMode, setEditMode] = useState(false);
 
   const handleChange = (e) => {
-    const value = e.target.value;
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
+    const { name, value } = e.target;
+    setUser(prevUser => ({
+      ...prevUser,
+      [name]: value
+    }));
   };
 
   const handleRole = (e) => {
     const newRole = e.target.value;
-    setUser((prevUser) => {
-      const updatedUser = { ...prevUser, role: newRole };
-      console.log(newRole);
-      console.log(updatedUser);
-      return updatedUser;
-    });
+    setUser(prevUser => ({
+      ...prevUser,
+      role: newRole
+    }));
   };
 
   const handleEdit = () => {
@@ -59,118 +75,200 @@ const UserProfileEdit = () => {
   const handleSave = () => {
     console.log(user);
     axios.post(`http://127.0.0.1:8000/api/update-user/${user.id}`, user)
-        .then(response => {
-          setUser(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
-      setTimeout(() => {
-        window.location.reload(false);
-      }, 1000);
+      .then(response => {
+        setUser(response.data);
+        setEditMode(false); // Thay đổi editMode về giá trị ban đầu
+      })
+      .catch(error => {
+        console.error(error);
+      });
   };
 
   const handleImageChange = (e) => {
-    const file = e.target.files[0]
+    const file = e.target.files[0];
     console.log(URL.createObjectURL(file));
-    // reader.onload = () => {
-    //   setUser({
-    //     ...user,
-    //     img: ,
-    //   });
+
   };
 
   return (
-    <div className="user-profile-edit">
-      <div className="profile-image">
-        <img src={user.img} alt="Profile" />
-        {editMode && (
-          <input
-            type="file"
-            onChange={handleImageChange}
-          />
-        )}
-      </div>
-      <div className="user-info">
-        <h2>My Profile</h2>
-        {editMode ? (
-          <form>
-            <label>
-              Name:
-              <input
-                type="text"
-                name="name"
-                value={user.name}
-                onChange={handleChange}
-              />
-            </label>
-            <br />
-            <label>
-              Phone:
-              <input
-                type="text"
-                name="phone"
-                value={user.phone}
-                onChange={handleChange}
-              />
-            </label>
-            <br />
-            <label>
-              Email:
-              <input
-                type="email"
-                name="email"
-                value={user.email}
-                onChange={handleChange}
-              />
-            </label>
-            <br />
-            <label>
-              Address:
-              <input
-                type="text"
-                name="address"
-                value={user.address}
-                onChange={handleChange}
-              />
-            </label>
-            <br />
-            <label>
-              <select value={user.role}  name="role" id="role" onChange={handleRole}>
-                <option value="teacher">Teacher</option>
-                <option value="student">Student</option>
-              </select>
-            </label>
-            <br />
-            <button type="button" onClick={handleSave}>
-              Save
-            </button>
-          </form>
-        ) : (
-          <>
-            <p>
-              <strong>Name:</strong> {user.name}
-            </p>
-            <p>
-              <strong>Phone:</strong> {user.phone}
-            </p>
-            <p>
-              <strong>Email:</strong> {user.email}
-            </p>
-            <p>
-              <strong>Address:</strong> {user.address}
-            </p>
-            <p>
-              <strong>Role:</strong> {user.role}
-            </p>
-            <button type="button" onClick={handleEdit}>
-              Edit
-            </button>
-          </>
-        )}
-      </div>
-    </div>
+    <section style={{ backgroundColor: '#eee' }}>
+      <MDBRow>
+        <MDBCol lg="4">
+          <MDBCard className="mb-4">
+            <MDBCardBody className="text-center">
+              <label htmlFor="image-upload" className="d-block cursor-pointer">
+                <MDBCardImage
+                  src={user.img}
+                  alt="avatar"
+                  className="rounded-circle"
+                  fluid
+                />
+              </label>
+              {editMode && (
+                <div className="d-flex justify-content-center mb-2">
+                  <input
+                    id="image-upload"
+                    type="file"
+                    accept="image/*"
+                    className="visually-hidden"
+                    onChange={handleImageChange}
+                  />
+                </div>
+              )}
+              <p className="text-muted mb-1">
+                {editMode ? (
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="jobTitle"
+                    value={user.name}
+                    onChange={handleChange}
+                  />
+                ) : (
+                  user.name
+                )}
+              </p>
+              <p className="text-muted mb-4">
+                {editMode ? (
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="role"
+                    value={user.role}
+                    onChange={handleChange}
+                  />
+                ) : (
+                  user.role
+                )}
+              </p>
+            </MDBCardBody>
+          </MDBCard>
+
+          <MDBCard className="mb-4 mb-lg-0">
+            <MDBCardBody className="p-0">
+              <MDBListGroup flush className="rounded-3">
+                <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
+                  <MDBIcon fas icon="at fa-lg text-warning" />
+                  <MDBCardText className="m-0"></MDBCardText>
+                  <MDBCardText className="m-0 text-muted">{user.name}</MDBCardText>
+                </MDBListGroupItem>
+                <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
+                  <MDBIcon fas icon="phone fa-lg text-warning" />
+                  <MDBCardText className="m-0"></MDBCardText>
+                  <MDBCardText className="m-0 text-muted">{user.phone}</MDBCardText>
+                </MDBListGroupItem>
+                <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
+                  <MDBIcon fas icon="mobile-alt fa-lg text-warning" />
+                  <MDBCardText className="m-0"></MDBCardText>
+                  <MDBCardText className="m-0 text-muted">{user.email}</MDBCardText>
+                </MDBListGroupItem>
+                <MDBListGroupItem className="d-flex justify-content-between align-items-center p-3">
+                  <MDBIcon fas icon="map-marker-alt fa-lg text-warning" />
+                  <MDBCardText className="m-0"></MDBCardText>
+                  <MDBCardText className="m-0 text-muted">{user.address}</MDBCardText>
+                </MDBListGroupItem>
+              </MDBListGroup>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+
+        <MDBCol lg="8">
+          <MDBCard className="mb-4">
+            <MDBCardBody>
+              <MDBCardText className="h4 mb-4">Personal Information</MDBCardText>
+              <hr></hr>
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Name :</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  {editMode ? (
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="fullName"
+                      value={user.name}
+                      onChange={handleChange}
+                    />
+                  ) : (
+                    <MDBCardText>{user.name}</MDBCardText>
+                  )}
+                </MDBCol>
+              </MDBRow>
+              <hr></hr>
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Email :</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  {editMode ? (
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="email"
+                      value={user.email}
+                      onChange={handleChange}
+                    />
+                  ) : (
+                    <MDBCardText>{user.email}</MDBCardText>
+                  )}
+                </MDBCol>
+              </MDBRow>
+              <hr></hr>
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Phone :</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  {editMode ? (
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="phone"
+                      value={user.phone}
+                      onChange={handleChange}
+                    />
+                  ) : (
+                    <MDBCardText>{user.phone}</MDBCardText>
+                  )}
+                </MDBCol>
+              </MDBRow>
+
+              <hr></hr>
+              <MDBRow>
+                <MDBCol sm="3">
+                  <MDBCardText>Address :</MDBCardText>
+                </MDBCol>
+                <MDBCol sm="9">
+                  {editMode ? (
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="address"
+                      value={user.address}
+                      onChange={handleChange}
+                    />
+                  ) : (
+                    <MDBCardText>{user.address}</MDBCardText>
+                  )}
+                </MDBCol>
+              </MDBRow>
+              <hr></hr>
+              {editMode ? (
+                <MDBBtn onClick={handleSave} color="success" className="me-2">
+                  Save
+                </MDBBtn>
+              ) : (
+                <MDBBtn onClick={handleEdit} color="primary" className="me-2">
+                  Edit
+                </MDBBtn>
+              )}
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </section>
   );
 };
 
-export default UserProfileEdit;
+export default UserProfile;
--- reactjs/src/routes/index.js
+++ reactjs/src/routes/index.js
@@ -4,12 +4,15 @@ import Home from "../pages/Home/";
 import Login from "../pages/Login/Login";
 import Register from '../pages/Register/index';
 import UserProfile from "../pages/Profile/Userprofile";
+import ProfilePage from "../pages/Profile/Userprofile";
+
 
 const publicRoutes = [
   { path: "/", component: Home },
   { path: "/Login", component: Login, layout: WithoutLayout },
   { path: "/Register", component: Register, layout: WithoutLayout },
-  { path: "/user", component: UserProfile}
+  { path: "/user", component: UserProfile}, 
+  { path: "/users", component: ProfilePage}
 ];
 
 export { publicRoutes };
