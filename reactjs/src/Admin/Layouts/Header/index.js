eced1daeadbd3477c262d847c6fd90700269c647
hoxuanty <hoxuanty2003qng@gmail.com>
2023-07-06 22:51:03 +0700

    feat: handle logout admin page

--- reactjs/src/Admin/Layouts/Header/index.js
+++ reactjs/src/Admin/Layouts/Header/index.js
@@ -1,6 +1,39 @@
+import axios from 'axios';
 import React from 'react'
+import { AuthContext } from '../../../components/AuthContext';
+import { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+    const { user, setUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        setTimeout(() => {
+            window.location.reload(false)
+        }, 1500);
+        navigate('/login'); 
+    };
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        console.log(token);
+        if (token) {
+            axios.get('http://127.0.0.1:8000/api/user', {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+                .then(response => {
+                    setUser(response.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+    }, []);
+
     return (
         <>
             <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
@@ -19,8 +52,7 @@ function Header() {
                         </div>
                     </div>
                 </form>
-                {/* Topbar Navbar */}
-                <ul className="navbar-nav ml-auto">
+                {user &&(<ul className="navbar-nav ml-auto">
                     {/* Nav Item - Search Dropdown (Visible Only XS) */}
                     <li className="nav-item dropdown no-arrow d-sm-none">
                         <a className="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -43,19 +75,20 @@ function Header() {
                     {/* Nav Item - User Information */}
                     <li className="nav-item dropdown no-arrow">
                         <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span className="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                            <img className="img-profile rounded-circle" src="img/undraw_profile.svg" />
+                            <span className="mr-2 d-none d-lg-inline text-gray-600 small">{user.name}</span>
+                            <img className="img-profile rounded-circle" src={user.img} />
                         </a>
                         {/* Dropdown - User Information */}
                         <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                             <div className="dropdown-divider" />
-                            <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                            <a onClick={handleLogout} className="dropdown-item" data-toggle="modal" data-target="#logoutModal">
                                 <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400" />
                                 Logout
                             </a>
                         </div>
                     </li>
                 </ul>
+                )}
             </nav>
         </>
     )
